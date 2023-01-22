c9e905e478026bd8098edf1a02d71f9a59f92fa9
Oscar Dartigues <oscardartigues92@gmail.com>
2023-01-22 23:20:33 +0100

    delete useless files

--- src/router/index.js
+++ src/router/index.js
@@ -26,14 +26,7 @@ const routes = [
     name: 'call',
     component: CallView
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+
 ]
 
 const router = createRouter({
